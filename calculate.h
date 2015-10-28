227efa4304f0d3306b03c6b57f816fefd3fa06af
BabichMikhail <chessvlad2011@yandex.ru>
2015-10-28 22:00:07 +1000

    Calculate const expressions
    ArrayIndex

--- calculate.h
+++ calculate.h
@@ -11,6 +11,7 @@ private:
 	SymTable* Table;
 	T CalculateUnarConstExpr(ExprUnarOp* Exp);
 	T CalculateBinConstExpr(ExprBinOp* Exp);
+	T CalculateArrayIndex(ArrayIndex* Exp);
 	template <class X> void CheckType();
 public:
 	T Ans;
@@ -25,9 +26,6 @@ template <class T> T CalculateConstExpr<T>::Calculate(Expr* Exp) {
 	case VarExp:
 		return Calculate(((SymVar*)Table->GetSymbol(((ExprVar*)Exp)->Var.Source.c_str()))->InitExp);
 	
-	/*case ArrayExp:
-		return ((ArrayIndex*)Exp)->Right*/
-
 	case ConstIntExp:
 		CheckType<int>();
 		return atoi(((ExprIntConst*)Exp)->Value.Source.c_str());
@@ -43,6 +41,9 @@ template <class T> T CalculateConstExpr<T>::Calculate(Expr* Exp) {
 
 	case UnarExp:
 		return CalculateUnarConstExpr((ExprUnarOp*)Exp);
+
+	case ArrayExp:
+		return CalculateArrayIndex((ArrayIndex*)Exp);
 
 	default:
 		throw BadType();
@@ -95,6 +96,21 @@ template <class T> T CalculateConstExpr<T>::CalculateBinConstExpr(ExprBinOp* Exp
 	default:
 		throw BadType();
 	}
+}
+
+template <class T> T CalculateConstExpr<T>::CalculateArrayIndex(ArrayIndex* Exp) {
+	Expr* IdentExp = Exp;
+	vector<int> idxs;
+	while (IdentExp->TypeExp != VarExp) {
+		idxs.push_back(Calculate(((ArrayIndex*)IdentExp)->Right));
+		IdentExp = ((ArrayIndex*)IdentExp)->Left;
+	}
+	InitList* ExpList = (InitList*)((SymConst*)Table->GetSymbol(((ExprVar*)IdentExp)->Var.Source.c_str()))->InitExp;
+	IdentExp = Exp;
+	for (int i = idxs.size() - 1; i >= 0; --i) { 
+		ExpList = (InitList*)ExpList->List[idxs[i]];
+	}
+	return Calculate(ExpList);
 }
 
 template <class T> template <class X> void CalculateConstExpr<T>::CheckType() {
